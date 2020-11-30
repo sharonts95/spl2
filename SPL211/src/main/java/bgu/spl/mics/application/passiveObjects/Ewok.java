462055f3050f7aa92430a461d36181a6aa4c5597
sharonts95 <63657477+sharonts95@users.noreply.github.com>
2020-11-30 15:23:55 +0200

    sharon

--- SPL211/src/main/java/bgu/spl/mics/application/passiveObjects/Ewok.java
+++ SPL211/src/main/java/bgu/spl/mics/application/passiveObjects/Ewok.java
@@ -7,21 +7,30 @@ package bgu.spl.mics.application.passiveObjects;
  * You may add fields and methods to this class as you see fit (including public methods).
  */
 public class Ewok {
-	int serialNumber;
-	boolean available;
-	
-  
+    int serialNumber;
+    boolean available;
+
+    public Ewok(int serialNumber){
+        available=true;
+        this.serialNumber=serialNumber;
+    }
+
+
     /**
      * Acquires an Ewok
      */
     public void acquire() {
-		
+        this.available=false;
     }
 
     /**
      * release an Ewok
      */
     public void release() {
-    	
+        this.available=true;
+    }
+
+    public boolean isAvailable(){
+        return available;
     }
 }
--- SPL211/src/test/java/bgu/spl/mics/EwokTest.java
+++ SPL211/src/test/java/bgu/spl/mics/EwokTest.java
@@ -1,4 +1,36 @@
 package bgu.spl.mics;
 
+import bgu.spl.mics.application.passiveObjects.Ewok;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class EwokTest {
+
+    private Ewok ewok;
+
+    @BeforeEach
+    public void setUp(){ewok=new Ewok(0);}
+
+    @Test
+    public void testAcquire(){
+        ewok.acquire();
+        assertFalse(ewok.isAvailable());
+    }
+
+    @Test
+    public void testRelease(){
+        ewok.acquire();
+        ewok.release();
+        assertTrue(ewok.isAvailable());
+    }
+
 }
